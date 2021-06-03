815c3a69dbf2b742dda1d892fb14e3a1e89fe6ec
Nguyen Van Cuong <cuong.ktpm@gmail.com>
2021-06-03 11:57:10 +0700

    ! add navigation and add screens

--- src/App.js
+++ src/App.js
@@ -1,116 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Screen/HomeScreen'
+import DetailsScreen from './Screen/DetailsScreen'
 
-import React, { Component } from 'react';
-import type { Node } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TouchableOpacity,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator();
 
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home Screen" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-class App extends Component {
-  constructor(props) {
-    console.log("App", "constructor");
-    super(props);
-
-    this.state = {
-      result: 0
-    }
-
-  }
-  
-  _inCrease(){
-    console.log("App", "_inCrease");
-    this.setState({
-      result : this.state.result + 1
-    })
-  }
-
-  render() {
-    console.log("App", "render");
-
-    return (
-      <View styles={{ flex: 1, backgroundColor: 'white' }}>
-        <View styles={{ flex: 1, backgroundColor: 'blue', justifyContent: 'center', alignItems: 'center' }}>
-          <Text> {this.state.result} </Text>
-        </View>
-
-        <View styles={{ flex: 1, backgroundColor: 'skyblue', justifyContent: 'center', alignItems: 'center' }}>
-          <TouchableOpacity onPress={this._inCrease.bind(this)}>
-            <Text> Click me </Text>
-          </TouchableOpacity>
-
-        </View>
-      </View>
-    );
-  };
-
-
-
-  componentDidCatch() {
-    console.log("App", "componentDidCatch");
-  }
-
-  componentDidUpdate() {
-    console.log("App", "componentDidUpdate");
-  }
-
-  componentWillUnmount() {
-    console.log("App", "componentWillUnmount");
-  }
-
-  componentDidMount() {
-    console.log("App", "componentDidMount");
-  }
-
 }
 
-
-
-
-export default App;
---
+++ src/Screen/DetailsScreen.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+export default function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home Screen')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
